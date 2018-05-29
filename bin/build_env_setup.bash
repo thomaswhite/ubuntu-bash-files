6282ad49c5f6d4acba1e5a062284874828ffde5a
Thomas <thomas.0007@gmail.com>
2018-05-29 10:16:39 +0100

    Add build enviroment setup.

--- bin/build_env_setup.bash
+++ bin/build_env_setup.bash
@@ -15,9 +15,6 @@ pushd $download_path
 wget -q https://raw.githubusercontent.com/thomaswhite/ubuntu-bash-files/master/.bash_functions_git
 wget -q https://raw.githubusercontent.com/thomaswhite/ubuntu-bash-files/master/.bash_functions_ppa-deb
 wget -q https://raw.githubusercontent.com/thomaswhite/ubuntu-bash-files/master/.bash_functions_print
-
-
-
 source .bash_functions_git
 source .bash_functions_ppa-deb
 source .bash_functions_dir
